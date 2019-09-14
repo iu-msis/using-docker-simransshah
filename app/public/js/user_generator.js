6c3537ab91a0620ead4aaf36cdef1fcba9486487
Simran Shah <simransshah2@gmail.com>
2019-09-13 20:19:07 -0400

    Addition of Randomuser API, fetching data and display using VueJS

--- app/public/js/user_generator.js
+++ app/public/js/user_generator.js
@@ -5,16 +5,14 @@ data: {
 },
 methods :{
   fetchUsers(){
-    //fetch('https://randomuser.me/api/')
-    //.then( response => response.json())
-    //.then( json => {aboutMe.users = json})
+
     //Means the same at this
        fetch('https://randomuser.me/api/1.2')
        .then(function(response) {return response.json()})
        .then(function(json) {aboutMe.users = json});
 
     //.catch(/*function goes here*/)
-    ;
+
     //promise object is created. fetch works asynchronously
   }
 },
